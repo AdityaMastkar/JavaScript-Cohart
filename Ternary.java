ccbd4c8cb463b3dcc0a5352fa2c86f98f457862f
AdityaMastkar <adityamastkar94@gmail.com>
2025-03-01 20:00:38 +0530

    Second commit

--- Ternary.java
+++ Ternary.java
@@ -5,8 +5,7 @@ public class Ternary {
         // by using ternary operator
         String Aditya = ((val%2)==0) ? "even" : "odd";
         System.out.println(Aditya);
-        // int larger = (5>30)? 5 : 30;
-        // System.out.println(larger);
         
+
     }
 }
