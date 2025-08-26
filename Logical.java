2019d92328a2550a1ef8164df1c658f7c643e3c9
Aditya Mastkar <adityamastkar942gmail.com>
2025-08-26 20:35:32 +0530

    new

--- Logical.java
+++
@@ -1,7 +0,0 @@
-public class Logical {
-    public static void main (String [] args) {
-         System.out.println( (10>5) && (50<10) );
-         System.out.println((5<1) || (10<100));
-         System.out.println(!(5>40));
-    }
-}
--- PassOrFail.java
+++
@@ -1,7 +0,0 @@
-public class PassOrFail {
-    public static void main (String [] args) {
-        int marks = 33;
-        String Report = (marks>=32)? "Pass":"Fail";
-        System.out.println(Report);
-    }
-}
